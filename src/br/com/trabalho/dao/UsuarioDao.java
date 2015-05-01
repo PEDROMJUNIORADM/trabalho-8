5c7e9e0c515c39e792e7d95dc8e62b43cea723a6
Eric Bernardes <djeric14@gmail.com>
2015-05-01 00:33:58 -0300

    layout usuario-editar

--- src/br/com/trabalho/dao/UsuarioDao.java
+++ src/br/com/trabalho/dao/UsuarioDao.java
@@ -1,6 +1,5 @@
 package br.com.trabalho.dao;
 
-import java.util.Calendar;
 import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
@@ -13,7 +12,7 @@ import br.com.trabalho.model.Usuario;
 public class UsuarioDao extends GenericListDao<Usuario> {
 
 	public UsuarioDao() {
-		Calendar dataNascimento = Calendar.getInstance();
+		/**Calendar dataNascimento = Calendar.getInstance();
 		dataNascimento.set(2015, 1, 1);
 
 		Usuario usuario = new Usuario();
@@ -21,11 +20,11 @@ public class UsuarioDao extends GenericListDao<Usuario> {
 		usuario.setNomeReduzido("Admin");
 		usuario.setNomeCompleto("Administrador do Sistema");
 		usuario.setEmail("admin@admin.com");
-		usuario.setSenha("admin");
-		usuario.setCpf("03755871793");
+		usuario.setSenha("admin1");
+		usuario.setCpf("1");
 		usuario.setDataNascimento(dataNascimento.getTime());
 
-		super.salvar(usuario);
+		super.salvar(usuario); */
 	}
 
 	public void salvarUsuario(Usuario usuario) {
