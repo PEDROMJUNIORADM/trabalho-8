8722c74e30a6238caec799b2360e8ebd0466f366
Eric Bernardes <djeric14@gmail.com>
2015-04-30 21:30:58 -0300

    Conclusão das Validações e Conversões

--- src/br/com/trabalho/dao/UsuarioDao.java
+++ src/br/com/trabalho/dao/UsuarioDao.java
@@ -1,5 +1,6 @@
 package br.com.trabalho.dao;
 
+import java.util.Calendar;
 import java.util.List;
 
 import javax.faces.bean.ApplicationScoped;
@@ -12,7 +13,7 @@ import br.com.trabalho.model.Usuario;
 public class UsuarioDao extends GenericListDao<Usuario> {
 
 	public UsuarioDao() {
-		/**Calendar dataNascimento = Calendar.getInstance();
+		Calendar dataNascimento = Calendar.getInstance();
 		dataNascimento.set(2015, 1, 1);
 
 		Usuario usuario = new Usuario();
@@ -21,10 +22,10 @@ public class UsuarioDao extends GenericListDao<Usuario> {
 		usuario.setNomeCompleto("Administrador do Sistema");
 		usuario.setEmail("admin@admin.com");
 		usuario.setSenha("admin");
-		usuario.setCpf("1");
+		usuario.setCpf("03755871793");
 		usuario.setDataNascimento(dataNascimento.getTime());
 
-		super.salvar(usuario);*/
+		super.salvar(usuario);
 	}
 
 	public void salvarUsuario(Usuario usuario) {
